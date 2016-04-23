de457e788db7f9e768d97f678ac7c427918ea8d5
luturol <rafael.ahrons@gmail.com>
2016-04-23 16:40:44 -0300

    Refatorando jUnit Tutorial

--- src/jUnit/NumerosTeste.java
+++ src/jUnit/NumerosTeste.java
@@ -7,25 +7,26 @@ import org.junit.Test;
 
 public class NumerosTeste {
 	
-	private Numeros n;
+	private Numeros numero;
 	
 	@Before
 	public void setUp(){
-		n = new Numeros();
+		numero = new Numeros();
 	}
 	@Test
 	public void testNumeroPar() {
-		assertEquals(true, n.numeroPar(8));
+		assertTrue(numero.numeroPar(8));
 	}
 
 	@Test
 	public void testAreaQuadrado() {
-		assertEquals(100, n.areaQuadrado(10));
+		int area = 100;
+		assertEquals(area, numero.areaQuadrado(10));
 	}
 
 	@Test
-	public void testNumeroDivisivel() {
-		assertTrue(n.numeroDivisivel(4, 2));
+	public void shouldBeAbleToDivedeGiven4And2() {
+		assertTrue(numero.numeroDivisivel(4, 2));
 	}
 
 }
