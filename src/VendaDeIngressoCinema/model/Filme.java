d132031988d17f01d4f3acde0f5258603fe061e0
luturol <rafael.ahrons@gmail.com>
2016-04-23 17:01:39 -0300

    Filme

--- src/VendaDeIngressoCinema/model/Filme.java
+++ src/VendaDeIngressoCinema/model/Filme.java
@@ -1,4 +1,57 @@
+package VendaDeIngressoCinema.model;
 
 public class Filme {
-
+	
+	private String codigo;
+	private String nomeFilme;
+	private String genero;
+	private String sinopse;
+	
+	public Filme(String codigo, String nome, String genero, String sinopse){
+		this.codigo = codigo;
+		this.nomeFilme = nome;
+		this.genero = genero;
+		this.sinopse = sinopse;
+	}
+	
+	public void setCodigo(String codigo){
+		this.codigo = codigo;
+	}
+	
+	public String getCodigo(){
+		return this.codigo;
+	}
+	
+	public void setNome(String nome){
+		this.nomeFilme = nome;
+	}
+	
+	public String getNome(){
+		return this.nomeFilme;
+	}
+	
+	public void setGenero(String genero){
+		this.genero = genero;
+	}
+	
+	public String getGenero(){
+		return this.genero;
+	}
+	
+	public void setSinopse(String sinopse){
+		this.sinopse = sinopse;
+	}
+	
+	public String getSinopse(){
+		return this.sinopse;
+	}
+	
+	public String toString(){
+		String msg = "Código: " + this.getCodigo() + "\n"
+				   + "Nome: " + this.getNome() + "\n"
+				   + "Gênero: " + this.getGenero() + "\n"
+				   + "Sinopse: " + this.getSinopse();
+		return msg;
+		
+	}
 }
